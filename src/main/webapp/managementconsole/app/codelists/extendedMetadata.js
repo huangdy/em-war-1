091766033d625f77b569c349e6a9f1749b14c547
hurrelld <hurrelld@leidos.com>
2015-10-22 11:24:20 -0400

    Initial XchangeCore 2.0 Commit

---
+++ src/main/webapp/managementconsole/app/codelists/extendedMetadata.js
@@ -0,0 +1,23 @@
+[
+	{
+			"label": "CAP Certainty",
+			"codespace":"urn:oasis:names:tc:emergency:cap:1.2",
+			"code": "certainty"
+	},
+	{
+			"label":"CAP Severity",
+			"codespace":"urn:oasis:names:tc:emergency:cap:1.2",
+			"code":  "severity"
+	},
+	{
+			"label":"CAP Urgency",
+			"codespace":"urn:oasis:names:tc:emergency:cap:1.2",
+			"code":  "urgency"
+	},
+	{
+			"label":"CAP Restriction",
+			"codespace":"urn:oasis:names:tc:emergency:cap:1.2",
+			"code":  "restriction"
+	}
+	
+]
